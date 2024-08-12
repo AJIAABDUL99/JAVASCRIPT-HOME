f2f3f032c3571e3f7181743ebc9d5cf1d1c25b1f
AJIAABDUL99 <ajiamoha254@gmail.com>
2024-08-12 12:12:17 +0300

    my first commit

---
+++ question1.js
@@ -0,0 +1,5 @@
+//Create a variable called carName, assign the value Volvo to it.
+let carName ="Volvo";
+
+
+
---
+++ question4.js
@@ -0,0 +1,5 @@
+//Create a variable called z, assign x + y to it, and display the result in an alert box.
+var x = 10; 
+var y = 5;  
+var z = x + y;
+alert("The result of x + y is: " + z);
